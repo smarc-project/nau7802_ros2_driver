73b14af62b1ac27b6f2e5e2d1de5ab02eab113ae
Ozer Ozkahraman <ozkahramanozer@gmail.com>
2025-10-02 17:46:47 +0200

    publishing to ros

--- nau7802_ros2_driver/nau7802_driver.py
+++ nau7802_ros2_driver/nau7802_driver.py
@@ -2,12 +2,16 @@ import time
 import board
 from cedargrove_nau7802 import NAU7802
 
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int32
 
+# mostly copied from Adafruit example:
+#https://github.com/adafruit/CircuitPython_NAU7802/blob/main/examples/nau7802_simpletest.py
 
 # Instantiate 24-bit load sensor ADC; two channels, default gain of 128
 nau7802 = NAU7802(board.I2C(), address=0x2A, active_channels=1)
 
-https://github.com/adafruit/CircuitPython_NAU7802/blob/main/examples/nau7802_simpletest.py
 def zero_channel():
     """Initiate internal calibration for current channel.Use when scale is started,
     a new channel is selected, or to adjust for measurement drift. Remove weight
@@ -37,23 +41,45 @@ def read_raw_value(samples=2):
     return int(sample_sum / samples)
 
 
-# Instantiate and calibrate load cell inputs
-print("*** Instantiate and calibrate load cells")
-# Enable NAU7802 digital and analog power
-enabled = nau7802.enable(True)
-print("Digital and analog power enabled:", enabled)
+class NAU7802DriverNode:
+    def __init__(self, node: Node, freq: float = 10.0):
+        self.node = node
+        self.publisher = node.create_publisher(Int32, 'sensor/load_cell/raw', 10)
+        self.msg = Int32()
+        self.timer = node.create_timer(1.0 / freq, self.publish)
 
-print("REMOVE WEIGHTS FROM LOAD CELLS")
-time.sleep(3)
+    def publish(self):
+        self.msg.data = read_raw_value()
+        self.publisher.publish(self.msg)
+        self.node.get_logger().info(f'Publishing: {self.msg.data}')
 
-nau7802.channel = 1
-zero_channel()  # Calibrate and zero channel
 
 
-print("READY")
 
-### Main loop: Read load cells and display raw values
-nau7802.channel = 1
-while True:
-    value = read_raw_value()
-    print(f"Raw value: {value:7.0f}")
+def __main__():
+    print("Calibrating loadcell, please wait...")
+    enabled = nau7802.enable(True)
+    print("Digital and analog power enabled:", enabled)
+    print("REMOVE WEIGHTS FROM LOAD CELLS IN 3 SECONDS")
+    time.sleep(3)
+
+    nau7802.channel = 1
+    zero_channel()  # Calibrate and zero channel
+
+    print("Publishing...")
+
+    rclpy.init()
+    node = Node('nau7802_driver_node')
+    nau7802_driver_node = NAU7802DriverNode(node)
+
+    rclpy.spin(node)
+    
+    nau7802.enable(False)  # Disable ADC when done
+
+    node.destroy_timer(nau7802_driver_node.timer)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    __main__()
